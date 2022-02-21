55bc594f433c0bf3bc54256d0efcc67d1ccdae10
chaen24 <83809778+chaen24@users.noreply.github.com>
2022-02-21 16:55:02 +0800

    Add files via upload

---
+++ RoutingandNavigation/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'product-details', component: ProductDetailsComponent},
+  {path: '', redirectTo:'home', pathMatch: 'full' },
+  {path: '**', component: NotFoundComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, AboutComponent, ProductComponent, ProductDetailsComponent, ContactComponent, NotFoundComponent]
